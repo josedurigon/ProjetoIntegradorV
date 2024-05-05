b5c8dafa2339c614bf343dafc2d03549382bdb56
josedurigon <jose.durigon@outlook.com>
2024-05-05 18:26:34 -0300

    Repository User

---
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -0,0 +1,51 @@
+package com.example.demo.Service;
+
+import com.example.demo.Entities.User;
+import com.example.demo.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public boolean addUser(User user) {
+        if (user != null) {
+            userRepository.saveAndFlush(user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean updateUser(long id, User user) {
+        Optional<User> existingUserOptional = userRepository.findById(id);
+        if (existingUserOptional.isPresent()) {
+            User existingUser = existingUserOptional.get();
+            existingUser.setUserName(user.getUserName());
+            // Update other fields if needed
+            userRepository.save(existingUser);
+            return true;
+        } else {
+            // Handle not found scenario
+            return false;
+        }
+    }
+
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
+}
